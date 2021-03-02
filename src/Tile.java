3483be1a4d5c809dfa59ddfce91c3362f00cbef7
Jeremy Hub <jhubinge@macalester.edu>
2021-03-02 12:12:51 -0800

    Added basic board and tiles

--- src/Tile.java
+++ src/Tile.java
@@ -1,7 +1,6 @@
-import edu.macalester.graphics.Ellipse;
-import edu.macalester.graphics.GraphicsGroup;
-import edu.macalester.graphics.GraphicsObject;
-import edu.macalester.graphics.Rectangle;
+import java.awt.Color;
+
+import edu.macalester.graphics.*;
 
 public class Tile extends GraphicsGroup{
     private final int TYPE;
@@ -9,7 +8,54 @@ public class Tile extends GraphicsGroup{
     private Ellipse dotShape;
     private Rectangle wallShape;
 
-    public Tile() {}
-    private void addDot() {}
-    private void removeDot() {}
+    public Tile(int type) {
+        this.TYPE = type;
+        switch (type) {
+            case 0:
+                addDot();
+                break;
+            case 1:
+                makeWall();
+                break;
+            case 2:
+                makeGhostArea();
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    private void addDot() {
+        hasDot = true;
+        dotShape = new Ellipse(0, 0, PacMan.DOT_SIZE, PacMan.DOT_SIZE);
+        add(dotShape);
+    }
+
+    public void removeDot() {
+        if (hasDot == true) {
+            remove(dotShape);
+            hasDot = false;
+        }
+    }
+
+    private void makeWall() {
+        wallShape = new Rectangle(0, 0, PacMan.TILE_SIDE_LENGTH, PacMan.TILE_SIDE_LENGTH);
+        wallShape.setFillColor(Color.BLACK);
+        add(wallShape);
+    }
+    
+    private void makeGhostArea() {
+        wallShape = new Rectangle(0, 0, PacMan.TILE_SIDE_LENGTH, PacMan.TILE_SIDE_LENGTH);
+        wallShape.setFillColor(Color.RED);
+        add(wallShape);
+    }
+
+    public int getType() {
+        return TYPE;
+    }
+
+    public Boolean getHasDot() {
+        return hasDot;
+    }
 }
