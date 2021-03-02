282f2a02b4ac2640e0cef8269687f9b495e339e5
Jeremy Hub <jhubinge@macalester.edu>
2021-03-02 12:20:31 -0800

    added default constructors

--- src/Ghost.java
+++ src/Ghost.java
@@ -1,3 +1,9 @@
 public class Ghost extends Sprite{
-    public Point getNewDestination() {}
+    public Ghost(double initialX, double initialY, int initialDirection) {
+        super(initialX, initialY, initialDirection);
+        // TODO Auto-generated constructor stub
+    }
+
+    public Point getNewDestination() {
+    }
 }
