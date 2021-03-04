0c41b59ddaa0aa8aafa65c08223c15de47945ac1
Youssef Aithmad <77765058+jaedyxn@users.noreply.github.com>
2021-03-04 02:31:15 -0600

    project
    player png rotates as they turn around, still need to implement it for ghosts

--- src/Ghost.java
+++ src/Ghost.java
@@ -1,16 +1,30 @@
 import edu.macalester.graphics.*;
 import java.awt.Color;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ghost extends Sprite {
     private int count = 0;
     private Random rand = new Random();
+    private List<Image> ghostPNG = new ArrayList<>();
+    private final Image redGhost = new Image("red_ghost.png");
+    private final Image yellowGhost = new Image("yellow_ghost.png");
+    private final Image purpleGhost = new Image("purple_ghost.png");
+    private final Image greyGhost = new Image("grey_ghost.png");
 
     public Ghost(Point point, int initialDirection, Tile[][] board) {
         super(point, initialDirection, board);
-        Rectangle shape = new Rectangle(0, 0, 10, 10);
-        shape.setFillColor(Color.RED);
-        add(shape);
+        ghostPNG.add(redGhost);
+        ghostPNG.add(yellowGhost);
+        ghostPNG.add(purpleGhost);
+        ghostPNG.add(greyGhost);
+        Image ghost = ghostPNG.get(rand.nextInt(ghostPNG.size()));
+        ghost.setScale(0.05);
+        add(ghost);
+        // Rectangle shape = new Rectangle(0, 0, 10, 10);
+        // shape.setFillColor(Color.RED);
+        // add(shape);
     }
 
     public void updatePos() {
@@ -27,6 +41,8 @@ public class Ghost extends Sprite {
                 break;
             }
             super.changeDirection(random);
+            // if (random == 3) super.setRotation(); need to figure out a way to flip images along an axis
+            // if (random == 1) super.setRotation();
             count = 0;
         }
         super.updatePos();
--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,13 @@
-import edu.macalester.graphics.Rectangle;
+// import edu.macalester.graphics.Rectangle;
 
 import edu.macalester.graphics.*;
 
 public class Player extends Sprite {
     public Player(Point point, int initialDirection, Tile[][] board) {
         super(point, initialDirection, board);
-        add(new Rectangle(0, 0, 10, 10));
+        Image player = new Image (0, 0, "pacman.gif");
+        player.setScale(0.5);
+        add(player);
+        // add(new Rectangle(0, 0, 10, 10));
     }
 }
